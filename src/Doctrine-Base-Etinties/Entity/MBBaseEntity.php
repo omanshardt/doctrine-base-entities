0f604f7841fe5481abb76cbe58499f4f75de4cc4
omanshardt <12620@manshardt.de>
2018-04-16 09:18:14 +0200

    Initial commit

---
+++ src/Doctrine-Base-Etinties/Entity/MBBaseEntity.php
@@ -0,0 +1,93 @@
+<?php
+namespace Omanshardt\DBE\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+/**
+ * MBBaseEntity
+ *
+ * @ORM\MappedSuperclass
+ * @ORM\HasLifecycleCallbacks()
+ */
+class MBBaseEntity
+{
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime", nullable=false)
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modified", type="datetime", nullable=false)
+     */
+    private $modified;
+
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Entity
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return \DateTime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     *
+     * @return Entity
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedValue(LifecycleEventArgs $event) {
+        $this->created = new \DateTime();
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function setupdatedValue(LifecycleEventArgs $event) {
+        $this->modified = new \DateTime();
+    }
+}
